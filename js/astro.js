bad19503cfca522a37a2088aa3e19e759a42b4ab
Rohan Bhobe <rohan.bhobe@gmail.com>
2013-03-02 19:34:45 -0800

    housecleaning some stupid shit

--- js/astro.js
+++ js/astro.js
@@ -1,27 +1,135 @@
+var AstroApp = {
+    Models: {},
+    Collections: {},
+    Views: {}, 
+    Templates: {}
+};
 
-/* 
-AstroApp.Models.ItemGroup = Backbone.Model.extend({
+
+AstroApp.Models.Item = Backbone.Model.extend({
 
     defaults: {
-        name: 'Item Group Name',
-        items: new AstroApp.Collections.Items([])
+        name: 'Item Name', 
+        packed: false
     },
 
     initialize: function() {
-        //console.log('making an item section model');
+        
+    },
+
+    togglePacked: function() {
+        this.set({ packed: !this.get('packed') });
     }
 
 });
-*/
 
-//var itemGroup = new AstroApp.Models.ItemGroup({ name: 'toiletries', items: toiletryItems });
-// var itemGroupView = new AstroApp.Views.ItemGroupView({ model: itemGroup });
+AstroApp.Views.ItemView = Backbone.View.extend({
 
-// itemGroupView.render();
+    tagName: 'tr',
+    className: 'item-row',
+    type: 'ItemView', // for debugging
 
-//itemGroup.on('change', myCallback);
+    template: _.template($('#item-template').html()),
 
-//itemGroup.set({ name: 'gear' });
+    events: {
+        "click .pack-action" : "packItem"
+    },
 
-//console.log(itemGroup.get('name'));
-//console.log(itemGroup.get('items'));
+    initialize: function() {
+        this.model.on('change', this.modelChanged, this);
+    },
+
+    render: function () {
+        var itemHtml = this.template(this.model.toJSON());
+        this.$el.html(itemHtml);
+        return this;
+    },
+
+    modelChanged: function(model) {
+        // re-render the model's view when the model changes
+        this.render();
+    },
+
+    packItem: function() {
+        this.model.togglePacked(); // mark item as packed
+        console.log(this.model.get('name')+"'s state is now: packed="+this.model.get('packed'));
+        // todo: move the item out of the "to pack" collection into the "packed" collection
+    }
+
+});
+
+
+AstroApp.Collections.ItemGroup = Backbone.Collection.extend({
+
+    model: AstroApp.Models.Item,
+
+    initialize: function(models, options) {
+        // storing meta info for the collection, like name
+        this._meta = {};
+        _.each(options, this.set, this);
+    },
+
+    set: function(val, prop) {
+        this._meta[prop] = val;
+    },
+
+    get: function(prop) {
+        return this._meta[prop];
+    }
+
+});
+
+AstroApp.Views.ItemGroupView = Backbone.View.extend({
+    
+    groupList: '',
+
+    main: '#main',
+
+    type: 'ItemGroupView', // for debugging
+
+    template: _.template($('#item-group-template').html()),
+
+    // collection: passed in during init
+
+    initialize: function(options) {
+        
+        //this.model.on('change', this.render, this);
+
+        // todo: construct the groupList property from the item group name
+        // gear => gear-items-list
+        // Extra Stuff => extra-stuff-items-list
+        this.groupList = '#'+this.collection.get('name')+'-items-list';
+
+        //console.log(this.collection.get('name'));
+
+    },
+
+    processItem: function(item) {
+        var itemView = new AstroApp.Views.ItemView({ model: item });
+        $(this.groupList).append(itemView.render().el);
+    },
+
+    render: function() {
+
+        var itemGroupHtml = this.template({ name: this.collection.get('name') });
+        $(this.main).html(itemGroupHtml);
+
+        _.each(this.collection.models, this.processItem, this);
+
+        return this;
+    }
+    
+});
+
+var myToiletryItems = [
+    { 'name': 'toothbrush' },
+    { 'name': 'deodorant' },
+    { 'name': 'toothpaste' },
+];
+
+var toiletryItems = new AstroApp.Collections.ItemGroup( myToiletryItems, { name:'toiletries' } );
+
+var toiletryGroupView = new AstroApp.Views.ItemGroupView({ collection: toiletryItems });
+
+toiletryGroupView.render();
+

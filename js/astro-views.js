4ad2a8041e9be90b79012c06212b9e86bb249ba8
Rohan Bhobe <rohan.bhobe@gmail.com>
2013-03-07 21:36:52 -0800

    moving views out into their own file

---
+++ js/astro-views.js
@@ -0,0 +1,88 @@
+var AstroApp = {
+    Models: {},
+    Collections: {},
+    Views: {}, 
+    Templates: {}
+};
+
+AstroApp.Views.ItemView = Backbone.View.extend({
+
+    tagName: 'tr',
+    className: 'item-row',
+    type: 'ItemView', // for debugging
+
+    template: _.template($('#item-template').html()),
+
+    events: {
+        "click .pack-action" : "packItem"
+    },
+
+    initialize: function() {
+        this.model.on('change', this.modelChanged, this);
+    },
+
+    render: function () {
+        var itemHtml = this.template(this.model.toJSON());
+        this.$el.html(itemHtml);
+        return this;
+    },
+
+    modelChanged: function(model) {
+        // re-render the model's view when the model changes
+        this.render();
+    },
+
+    packItem: function() {
+        this.model.togglePacked(); // mark item as packed
+        console.log(this.model.get('name')+"'s state is now: packed="+this.model.get('packed'));
+        // todo: move the item out of the "to pack" collection into the "packed" collection
+    }
+
+});
+
+
+AstroApp.Views.ItemGroupView = Backbone.View.extend({
+    
+    groupListId: '',
+
+    main: '#main',
+
+    type: 'ItemGroupView', // for debugging
+
+    template: _.template($('#item-group-template').html()),
+    
+    initialize: function(options) {
+
+        this.collection.bind('reset', this.loaded, this);
+        this.collection.fetch({
+            success: function(collection, response, options) {
+                console.log('successful collection fetch');
+            },
+            error: function(col, xhr, opt) {
+                alert('error fetching collection');
+            }
+        });        
+
+    },
+
+    loaded: function() {
+        // set the groupListId once the selector name has been set for the collection
+        this.groupListId = '#'+this.collection.get('selectorName')+'-items-list';
+        this.render();
+    },
+
+    addItem: function(item) {
+        var itemView = new AstroApp.Views.ItemView({ model: item });
+        $(this.groupListId).append(itemView.render().el);
+    },
+
+    render: function() {
+        var itemGroupHtml = this.template({ name: this.collection.get('displayName') });
+        $(this.main).html(itemGroupHtml);
+
+        _.each(this.collection.models, this.addItem, this);
+
+        return this;
+    }
+    
+});
--- js/astro.js
+++ js/astro.js
@@ -1,10 +1,3 @@
-var AstroApp = {
-    Models: {},
-    Collections: {},
-    Views: {}, 
-    Templates: {}
-};
-
 
 AstroApp.Models.Item = Backbone.Model.extend({
 
@@ -17,41 +10,6 @@ AstroApp.Models.Item = Backbone.Model.extend({
 
     togglePacked: function() {
         this.set({ packed: !this.get('packed') });
-    }
-
-});
-
-AstroApp.Views.ItemView = Backbone.View.extend({
-
-    tagName: 'tr',
-    className: 'item-row',
-    type: 'ItemView', // for debugging
-
-    template: _.template($('#item-template').html()),
-
-    events: {
-        "click .pack-action" : "packItem"
-    },
-
-    initialize: function() {
-        this.model.on('change', this.modelChanged, this);
-    },
-
-    render: function () {
-        var itemHtml = this.template(this.model.toJSON());
-        this.$el.html(itemHtml);
-        return this;
-    },
-
-    modelChanged: function(model) {
-        // re-render the model's view when the model changes
-        this.render();
-    },
-
-    packItem: function() {
-        this.model.togglePacked(); // mark item as packed
-        console.log(this.model.get('name')+"'s state is now: packed="+this.model.get('packed'));
-        // todo: move the item out of the "to pack" collection into the "packed" collection
     }
 
 });
@@ -88,55 +46,7 @@ AstroApp.Collections.ItemGroup = Backbone.Collection.extend({
 
 });
 
-AstroApp.Views.ItemGroupView = Backbone.View.extend({
-    
-    groupListId: '',
-
-    main: '#main',
-
-    type: 'ItemGroupView', // for debugging
-
-    template: _.template($('#item-group-template').html()),
-    
-    initialize: function(options) {
-
-        this.collection.bind('reset', this.loaded, this);
-        this.collection.fetch({
-            success: function(collection, response, options) {
-                console.log('successful collection fetch');
-            },
-            error: function(col, xhr, opt) {
-                alert('error fetching collection');
-            }
-        });        
-
-    },
-
-    loaded: function() {
-        // set the groupListId once the selector name has been set for the collection
-        this.groupListId = '#'+this.collection.get('selectorName')+'-items-list';
-        this.render();
-    },
-
-    addItem: function(item) {
-        var itemView = new AstroApp.Views.ItemView({ model: item });
-        $(this.groupListId).append(itemView.render().el);
-    },
-
-    render: function() {
-        var itemGroupHtml = this.template({ name: this.collection.get('displayName') });
-        $(this.main).html(itemGroupHtml);
-
-        _.each(this.collection.models, this.addItem, this);
-
-        return this;
-    }
-    
-});
-
 var itemList = new AstroApp.Collections.ItemGroup();
 var itemListView = new AstroApp.Views.ItemGroupView({ collection: itemList });
-
-//toiletryGroupView.render();
 
 
